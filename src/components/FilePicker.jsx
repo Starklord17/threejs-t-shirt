0ac8fcb92dd2702efd21339bec1d925b1846ce3c
MaxSM <maxi.sm117@gmail.com>
2023-09-06 10:48:41 -0300

    :hammer: bug fix
    fix lint error regarding the missing file prop in props validation

--- src/components/FilePicker.jsx
+++ src/components/FilePicker.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-import CustomButton from './CustomButton'
+import CustomButton from "./CustomButton";
 
 /**
  * The `FilePicker` component is a React component that allows users to upload a file and provides
@@ -11,11 +12,11 @@ const FilePicker = ({ file, setFile, readFile }) => {
   return (
     <div className="filepicker-container">
       <div className="flex-1 flex flex-col">
-        <input 
+        <input
           id="file-upload"
           type="file"
           accept="image/*"
-            /* The `onChange` event handler in the `input` element is responsible for updating the `file` state
+          /* The `onChange` event handler in the `input` element is responsible for updating the `file` state
             when a file is selected by the user. */
           onChange={(e) => setFile(e.target.files[0])}
         />
@@ -24,26 +25,32 @@ const FilePicker = ({ file, setFile, readFile }) => {
         </label>
 
         <p className="mt-2 text-gray-500 text-xs truncate">
-          {file === '' ? "No file selected" : file.name}
+          {file === "" ? "No file selected" : file.name}
         </p>
       </div>
 
       <div className="mt-4 flex flex-wrap gap-3">
-        <CustomButton 
+        <CustomButton
           type="outline"
           title="Logo"
-          handleClick={() => readFile('logo')}
+          handleClick={() => readFile("logo")}
           customStyles="text-xs"
         />
-        <CustomButton 
+        <CustomButton
           type="filled"
           title="Full"
-          handleClick={() => readFile('full')}
+          handleClick={() => readFile("full")}
           customStyles="text-xs"
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FilePicker
+FilePicker.propTypes = {
+  file: PropTypes.object,
+  setFile: PropTypes.func.isRequired,
+  readFile: PropTypes.func.isRequired,
+};
+
+export default FilePicker;
